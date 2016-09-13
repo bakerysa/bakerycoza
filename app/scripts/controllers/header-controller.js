441bddaca69701598ad74178566ea1c4aa891866
IXDCOZA <dylan@ixd.co.za>
2016-09-13 16:24:41 +0200

    header scroll animations

--- app/scripts/controllers/header-controller.js
+++ app/scripts/controllers/header-controller.js
@@ -20,7 +20,45 @@
 					$scope.theme2 = function (){
 						$rootScope.primaryColor = '#fff';
 					};
-					
+
+					var tl = new TimelineMax({
+					    paused: true
+					});
+
+					tl.to(".header", 0.3, {
+					    top: 0,
+					    css: {
+					    	'padding-top': '10px', 
+					    	'padding-bottom': '10px',
+					    },
+					    ease: Circ.easeInOut
+					})
+					.to(".header", 0.3, {
+					    backgroundColor : '#f3cbb7',
+					    ease: Circ.easeInOut
+					}, "=-0.3")
+
+					.to(".js-menu-item", 0.3, {
+						css: {
+							'padding-top': '1.8em',
+							'padding-bottom': '1.1em',
+						}
+					}, "=-0.3")
+
+					.to(".js-logo", 0.3, {
+					    height: 70,
+					    ease: Circ.easeInOut
+					}, "=-0.3");
+
+
+					$document.scroll(function() {
+					  if ($document.scrollTop() >= 50) {
+					  	tl.play();
+					  } else {
+					  	tl.reverse();
+					  }
+					});
+
 
 	  }]);
 
