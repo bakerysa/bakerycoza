ca920accba6b7442925e8aae5168ca3222572e24
IXDCOZA <dylan@ixd.co.za>
2016-10-18 18:38:58 +0200

    What We Do progress

--- app/scripts/controllers/work-controller.js
+++ app/scripts/controllers/work-controller.js
@@ -10,27 +10,26 @@
 	 * Controller of the bakeryApp
 	 */
 	angular.module('bakeryApp')
-        .controller('WorkCtrl', ['$scope', '$state', '$document', 'ProjectService', function($scope, $state,  $document, ProjectService){
+        .controller('WorkCtrl', ['$scope', '$timeout', 'ProjectService', function($scope, $timeout, ProjectService){
 
             $scope.projects = ProjectService.list();
 
 
+            $('.js-eff-work-img').duotone({
+               gradientMap: '#000000, #ecede0'
+            });
 
-            	$('.js-eff-work-img').duotone({
-            	   gradientMap: '#000000, #ecede0'
-            	});
-
-            	$('.js-eff-wwd-img').duotone({
-            	   gradientMap: '#000000, #fff5de'
-            	});
-
-
-            	$('#creative-partners img').duotone({
-            	   gradientMap: '#000000, #fde9eb'
-            	});
+            $('.js-eff-wwd-img').duotone({
+               gradientMap: '#000000, #fff5de'
+            });
 
 
+            $('#creative-partners img').duotone({
+               gradientMap: '#000000, #fde9eb'
+            });
 
+
+            
 	  }]);
 
   })(); 
